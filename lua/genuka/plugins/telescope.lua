ec9fca469ab1c133e70a095695d3ce39568ba1d1
7strickes <genukawijenayake@gmail.com>
2025-03-30 21:45:57 -0400

    chaning keybinding

--- lua/genuka/plugins/telescope.lua
+++ lua/genuka/plugins/telescope.lua
@@ -47,6 +47,6 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    keymap.set("n", "<leader>fC", "<cmd>Telescope find_files cwd=~/.config/nvim<cr>", { desc = "Find todos" })
+    keymap.set("n", "<leader>fC", "<cmd>Telescope find_files cwd=~/.config/nvim<cr>", { desc = "Nvim configs" })
   end,
 }
