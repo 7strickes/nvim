2f7f784bde42a0b6a5620cd7e251a857ccd87b33
7strickes <genukawijenayake@gmail.com>
2025-04-27 12:44:28 -0400

    alpha.nvim
    better alpha nvim ui configuration

--- lua/genuka/plugins/alpha.lua
+++ lua/genuka/plugins/alpha.lua
@@ -31,31 +31,40 @@ return {
     }
 
     -- Custom Footer
-    dashboard.section.footer.val = {
-      "⚡ Neovim loaded "
-        .. require("lazy").stats().loaded
-        .. "/"
-        .. require("lazy").stats().count
-        .. " plugins in "
-        .. string.format("%.2f", require("lazy").stats().startuptime)
-        .. "ms",
-    }
-
+    -- dashboard.section.footer.val = {
+    --   "⚡ Neovim loaded "
+    --     .. require("lazy").stats().loaded
+    --     .. "/"
+    --     .. require("lazy").stats().count
+    --     .. " plugins in "
+    --     .. string.format("%.2f", require("lazy").stats().startuptime)
+    --     .. "ms",
+    -- }
+    dashboard.section.header.opts.hl = "AlphaHeader"
     -- Hide status line when Alpha is open
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "AlphaReady",
-      callback = function()
-        vim.opt.laststatus = 0 -- Hide status line
-      end,
-    })
+    -- vim.api.nvim_create_autocmd("User", {
+    --   pattern = "AlphaReady",
+    --   callback = function()
+    --     vim.opt.laststatus = 0 -- Hide status line
+    --   end,
+    -- })
 
     -- Restore status line when leaving Alpha
-    vim.api.nvim_create_autocmd("BufUnload", {
-      pattern = "<buffer>",
-      callback = function()
-        vim.opt.laststatus = 2 -- Restore status line
-      end,
-    })
-    alpha.setup(dashboard.config)
+    -- vim.api.nvim_create_autocmd("BufUnload", {
+    --   pattern = "<buffer>",
+    --   callback = function()
+    --     vim.opt.laststatus = 2 -- Restore status line
+    --   end,
+    -- })
+    -- alpha.setup(dashboard.config)
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = "AlphaButtons"
+      button.opts.hl_shortcut = "AlphaShortcut"
+    end
+    dashboard.section.header.opts.hl = "AlphaHeader"
+    dashboard.section.buttons.opts.hl = "AlphaButtons"
+    dashboard.section.footer.opts.hl = "AlphaFooter"
+    dashboard.opts.layout[1].val = 8
+    return dashboard
   end,
 }
