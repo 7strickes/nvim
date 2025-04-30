5e48cc888935849aab4a63e8b83483e5ac062111
strickes <genukawijenayake@gmail.com>
2025-04-30 12:42:22 -0400

    fixed remote scp error
    .git files in the local directory disabled copies to remote. moded
    function to exclude .git dir

--- lua/plugins/remote.lua
+++ lua/plugins/remote.lua
@@ -1,10 +1,28 @@
 return {
   "amitds1997/remote-nvim.nvim",
-  version = "*", -- Pin to GitHub releases
+  version = "*",
   dependencies = {
-    "nvim-lua/plenary.nvim", -- For standard functions
-    "MunifTanjim/nui.nvim", -- To build the plugin UI
-    "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-telescope/telescope.nvim",
   },
-  config = true,
+  config = function()
+    local config_path = vim.fn.stdpath("config")
+    local all_dirs = vim.fn.readdir(config_path)
+    local filtered_dirs = vim.tbl_filter(function(dir)
+      local stat = vim.loop.fs_stat(config_path .. "/" .. dir)
+      return stat and stat.type == "directory" and dir ~= ".git"
+    end, all_dirs)
+
+    require("remote-nvim").setup({
+      remote = {
+        copy_dirs = {
+          config = {
+            base = config_path,
+            dirs = filtered_dirs,
+          },
+        },
+      },
+    })
+  end,
 }
