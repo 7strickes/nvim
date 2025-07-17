3388c638b587a755d9fceefbff8ae59b859c5fb0
strickes <genukawijenayake@gmail.com>
2025-07-16 22:32:28 -0400

    imporoved the outline.lua plugin
    working as default with out lazyvim.nvim

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -9,29 +9,14 @@ return {
   },
   {
     "hedyhli/outline.nvim",
-    keys = { { "<leader>cr", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
-    cmd = "Outline",
-    opts = function()
-      local defaults = require("outline.config").defaults
-      local opts = {
-        symbols = {
-          icons = {},
-          filter = vim.deepcopy(LazyVim.config.kind_filter),
-        },
-        keymaps = {
-          up_and_jump = "<up>",
-          down_and_jump = "<down>",
-        },
-      }
-
-      for kind, symbol in pairs(defaults.symbols.icons) do
-        opts.symbols.icons[kind] = {
-          icon = LazyVim.config.icons.kinds[kind] or symbol.icon,
-          hl = symbol.hl,
-        }
-      end
-      return opts
-    end,
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { -- Example mapping to toggle outline
+      { "<leader>cr", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    },
+    opts = {
+      -- Your setup opts here
+    },
   },
 
   -- edgy integration
